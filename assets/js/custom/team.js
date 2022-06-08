09a12042935abce27854dc0080996decc71a2f41
Bardia Khosravi <bardiakhosravi95@gmail.com>
2022-06-08 17:07:05 -0500

    completed teams page

--- assets/js/custom/team.js
+++ assets/js/custom/team.js
@@ -7,13 +7,13 @@ $(document).ready(function(){
             member_bio = member_item['bio']
             member_image = member_item['image']
             
-            team_html += `<div class="card row d-flex flex-row col-12 col-md-6 mb-3 offset-md-3">
-                <div class="card-body p-0 col-sm-12 col-md-4 d-flex flex-column justify-content-center">
+            team_html += `<div class="card row d-flex flex-row col-12 col-md-6 mb-5 offset-md-3">
+                <div class="card-body p-0 col-sm-12 col-md-4 d-flex flex-column justify-content-start pt-2">
                     <div>
                         <img src="./assets/imgs/team/${member_image}">
                     </div>
                 </div>
-                <div class="card-body col-sm-12 col-md-8">
+                <div class="card-body col-sm-12 col-md-8 py-0">
                     <p class="card-text">
                         <h5>${member_name}</h5>
                     </p>
